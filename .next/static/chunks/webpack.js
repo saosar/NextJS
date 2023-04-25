c84f0337aa371a3c88b05b8b3689ed3b5b901c9f
soa <saraospina12@gmail.com>
2023-04-24 20:49:06 -0500

    feat(api): simple todolist with NEXT JS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b4281294e89f3cc6"; }
+/******/ 		__webpack_require__.h = function() { return "4bcb9d7f2e1f393d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,52 @@
-import {useState} from 'react'
+import { useState } from 'react';
 
-const index = () => {
+const Index = () => {
+  // Cambio al escribir en la barra
+  const [userInput, setUserInput] = useState('');
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
 
-    const[userInput, setUserInput] = useState('')
+  // Crear encima de otro list al dar click en Submit
+  const [todoList, setTodoList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodoList([userInput, ...todoList]);
+    setUserInput('');
+  };
 
-    const handleChange = (e) => {
-        e.preventDefault()
+  // Borrar un elemento de la lista
+  const handleDelete = (todo) => {
+    const updatedArr = todoList.filter((todoItem) => todoItem !== todo);
+    setTodoList(updatedArr);
+  };
 
-        setUserInput(e.target.value)
-        // console.log(userInput)
-    }
-    return (
-        <div>
-            <h1>Next JS Todo List</h1>
-            <form>
-                <input type="text" onChange={handleChange}/> <button>Submit</button>
-            </form>
-        </div>
+  return (
+    <div>
+      <h1>Next.js Todo List</h1>
+      <form>
+        <input
+          type="text"
+          value={userInput}
+          placeholder={'Enter a to do'}
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+      <ul>
+        {todoList.length >= 1 ? (
+          todoList.map((todo, idx) => (
+            <li key={idx}>
+              {todo}
+              <button onClick={() => handleDelete(todo)}>Delete</button>
+            </li>
+          ))
+        ) : (
+          <p>Add an element</p>
+        )}
+      </ul>
+    </div>
+  );
+};
 
-    )
-}
-
-export default index
+export default Index;
